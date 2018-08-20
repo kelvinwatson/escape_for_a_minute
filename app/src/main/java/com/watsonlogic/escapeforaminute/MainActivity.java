57dcaa08e3f68622a6aa69aedd895c2ee7819ae9
Watson,Kelvin <kelvincswatson@gmail.com>
2018-08-19 22:50:12 -0400

    drawer

--- app/src/main/java/com/watsonlogic/escapeforaminute/MainActivity.java
+++ app/src/main/java/com/watsonlogic/escapeforaminute/MainActivity.java
@@ -4,23 +4,59 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener
 {
     private static final int REQUEST_CODE_SETTINGS = 36;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        setSupportActionBar(findViewById(R.id.toolbar));
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view ->
+        {
+            // some action
+        });
+
+        DrawerLayout drawer = getDrawerLayout();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string
+            .navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        ((NavigationView)findViewById(R.id.nav_view)).setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        DrawerLayout drawer = getDrawerLayout();
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -40,6 +76,42 @@ public class MainActivity extends AppCompatActivity
                     REQUEST_CODE_SETTINGS);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera)
+        {
+            // Handle the camera action
+        }
+        else if (id == R.id.nav_gallery)
+        {
+
+        }
+        else if (id == R.id.nav_slideshow)
+        {
+
+        }
+        else if (id == R.id.nav_manage)
+        {
+
+        }
+        else if (id == R.id.nav_share)
+        {
+
+        }
+        else if (id == R.id.nav_send)
+        {
+
+        }
+
+        getDrawerLayout().closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -62,5 +134,11 @@ public class MainActivity extends AppCompatActivity
                 }
                 break;
         }
+    }
+
+    @Nullable
+    private DrawerLayout getDrawerLayout()
+    {
+        return findViewById(R.id.drawer_layout);
     }
 }
